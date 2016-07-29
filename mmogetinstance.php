6494e898f38d6bbad9714bbc04beced27a760bf9
Bill Giffen <bill.giffen@gmail.com>
2016-07-29 12:35:40 -0600

    Minor reorg of instance management
    Moved record deletion and creation associated with releasing an instance
    to mmoreleaseinstance.

--- mmogetinstance.php
+++ mmogetinstance.php
@@ -2,42 +2,28 @@
 include_once "mmoconnection.php";
 $mydata = json_decode(file_get_contents('php://input'));
 $server_address = $mydata->server_address;
-
-$stmt = $conn->prepare("SELECT MIN(port) FROM instances WHERE server_address = ? AND recycle = 1");
+$stmt = $conn->prepare("SELECT MIN(port), instance_id FROM instances WHERE server_address = ? AND recycle = 1 GROUP BY instance_id");
 $stmt->bind_param("s", $server_address);
 $stmt->execute();
-$stmt->bind_result( $port_to_use );
-if ($port_to_use == 0) {
+$stmt->bind_result( $port, $instance_id );
+if ($port == 0) {
 	$stmt = $conn->prepare("SELECT COUNT(*) FROM instances WHERE server_address = ? ");
 	$stmt->bind_param("s", $server_address);
 	$stmt->execute();
 	$stmt->bind_result( $port );
 	$port = $port + 50000;
 	$stmt = $conn->prepare("INSERT INTO instances (instance_id, server_address, port, recycle) VALUES (NULL, ?, ?, 0) ");
-} else {
-	
-	$stmt = $conn->prepare("SELECT instance_id FROM instances WHERE server_address = ? AND port = ? ");
-	$stmt->bind_param("si", $server_address, $port_to_use);
+	$stmt->bind_param("si", $server_address, $port);
+	$stmt->execute();
+	$stmt = $conn->prepare("SELECT instance_id FROM instances WHERE server_address = ? AND port = ?");
+	$stmt->bind_param("si", $server_address, $port);
 	$stmt->execute();
 	$stmt->bind_result( $instance_id );
-	
-	$stmt = $conn->prepare("DELETE FROM instances WHERE instance_id = ? ");
+} else {
+	$stmt = $conn->prepare("UPDATE instances (instance_id, recycle) VALUES (?, 0) ");
 	$stmt->bind_param("i", $instance_id);
-	$stmt->execute();
-	$port = $port_to_use;
-	
-	
-	$stmt = $conn->prepare("INSERT INTO instances (instance_id, server_address, port, recycle) VALUES (NULL, ?, ?, 0) ");
-	
-	
+	$stmt->execute();		
 }
-$stmt->bind_param("si", $server_address, $port);
-$stmt->execute();
-$stmt = $conn->prepare("SELECT instance_id FROM instances WHERE server_address = ? AND port = ?");
-$stmt->bind_param("si", $server_address, $port);
-$stmt->execute();
-$stmt->bind_result( $instance_id );
 echo json_encode(array('status'=>'OK', 'port'=>$port, 'instance_id'=>$instance_id ));
 $stmt->close();
-
 ?>
--- mmoreleaseinstance.php
+++ mmoreleaseinstance.php
@@ -2,10 +2,14 @@
 include_once "mmoconnection.php";
 $mydata = json_decode(file_get_contents('php://input'));
 $instance_id = $mydata->$instance_id;
-$stmt = $conn->prepare("UPDATE instances SET recycle = 1 WHERE instance_id = ?");
+$port = $mydata->$port;
+$server_address = $mydata->$server_address
+$stmt = $conn->prepare("DELETE FROM instances WHERE instance_id = ? ");
 $stmt->bind_param("i", $instance_id);
 $stmt->execute();
+$stmt = $conn->prepare("INSERT INTO instances (instance_id, server_address, port, recycle) VALUES (NULL, ?, ?, 0) ");
+$stmt->bind_param("si", $server_address, $port);
+$stmt->execute();
 echo json_encode(array('status'=>'OK' ));
-$stmt->close();
-			
+$stmt->close();		
 ?>
