b4db4d28c21472d43c65d7b168aa055e296f0cae
AngelWolf <angelwolf@missingworldsmedia.com>
2016-08-17 21:17:06 +0000

    Added chatserver address to mmogetserver.php.

--- mmogetserver.php
+++ mmogetserver.php
@@ -1,5 +1,5 @@
 <?php
 
-echo json_encode(array('status'=>'OK', 'address'=>'angelwolf.ddns.net'));
+echo json_encode(array('status'=>'OK', 'chatserver'=>'lair.thephoenixprojectmmo.com', 'address'=>'angelwolf.ddns.net'));
 
 ?>
